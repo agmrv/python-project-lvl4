1826e316a4c64b7c513ece41a92c67b4d08f7abe
agmrv <airatgum@gmail.com>
2021-04-10 20:59:18 +0500

    formatting

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -22,7 +22,6 @@ load_dotenv(BASE_DIR / ".env")
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
