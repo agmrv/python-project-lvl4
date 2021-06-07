5a70f8da50657ec71d427c32f267646dc14f738a
agmrv <airatgum@gmail.com>
2021-06-07 12:32:11 +0500

    update settings

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -13,7 +13,7 @@ import os
 from pathlib import Path
 
 from dotenv import load_dotenv
-import django_heroku
+# import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -140,4 +140,4 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 # Heroku settings
-django_heroku.settings(locals())
+# django_heroku.settings(locals())
